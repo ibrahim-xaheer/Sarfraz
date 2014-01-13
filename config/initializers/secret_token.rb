341eea8da7bb0f009347e96bad699077a848ee51
ibrahim.xaheer@gmail.com <ibrahim.xaheer@gmail.com>
2014-01-14 03:39:50 +0500

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sarfraz::Application.config.secret_key_base = 'bb97903acd5f130e365578f220276902791392a815fddaa0806ac32e79cdeb3faa17493c05a12819aa0b3488d165509915c8604eb3bf128d024c296244ad472b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Sarfraz::Application.config.session_store :cookie_store, key: '_Sarfraz_session'
